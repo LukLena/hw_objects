30a79b93243aa1a45f8038a3ad0c3ab8f62e64a1
LukLena <106184354+LukLena@users.noreply.github.com>
2022-10-22 22:01:25 +0300

    first loading

---
+++ task.js
@@ -0,0 +1,38 @@
+function Student(name, gender, age) {
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+}
+Student.prototype.setSubject = function(subjectName) {
+    return this.subject = subjectName ;
+  }
+Student.prototype.addMark = function (mark) {
+     
+        if(this.marks === undefined){
+            this.marks = []
+          this.marks.push(mark) ; 
+        } else {
+            this.marks.push(mark)
+            }   
+  return this.marks.length;
+    }
+
+
+ Student.prototype.addMarks = function (...marks) {
+
+    marks.forEach(mark => this.addMark(mark));
+ }
+ 
+ Student.prototype.getAverage = function(){
+    let sum = 0;
+    this.marks.forEach(mark => sum += mark)        
+  let  average = sum / this.marks.length;
+ console.log(average)
+     return average;
+} ;
+
+Student.prototype.exclude = function (reason) {
+   delete this.subject;
+   delete this.marks;
+ this.excluded = reason;
+}
